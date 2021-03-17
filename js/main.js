47041acb67f1be2032bce4bbdddabb480fcc323b
andrei_labadaev <53191183+ivolat05@users.noreply.github.com>
2021-03-17 18:14:04 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,26 +2,26 @@ $(function(){
     $('.slaider').slick({
         infinite: true,
         slidesToShow: 4,
-        
+        slidesToScroll: 4,
         prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow__left.png" alt="prev"></button>',
         nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow__right.png" alt="next"></button>',
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1095,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
                 }                
             },
             {
-                breakpoint: 600,
+                breakpoint: 839,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2,
                 }                
             },
             {
-                breakpoint: 480,
+                breakpoint: 579,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
@@ -32,6 +32,11 @@ $(function(){
     $('.carousel__inner').slick({
         arrows: false,
         dots: true 
-    });	
-
+    });
+    
+    $('.menu__burger').click(function(event){
+        $('.menu__list').toggleClass('active'),
+        $('.menu__burger').toggleClass('burger__active')
+    });
+    
 });
